17ec4bcbb9de4194ee401a032e27de93527d7fbe
awwal148 <adesaanyaawwal446@gmail.com>
2023-12-17 11:53:20 -0800

    made some changes

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ hamburger.addEventListener("click",()=>{
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",()=>{
         hamburger.classList.remove("active")
-        navMenu.classList.romove("active")
+        navMenu.classList.remove("active")
         li.classList.remove("active")
     }))
 
